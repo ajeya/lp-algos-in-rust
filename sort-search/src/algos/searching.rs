b0bd640612753a8a125caf4d9a37c0bb73fa3a17
Ajeya Vempati <ajeya@works.sh>
2024-01-15 17:33:03 +1100

    implmented binary search

--- sort-search/src/algos/searching.rs
+++ sort-search/src/algos/searching.rs
@@ -1,6 +1,6 @@
 pub mod linear {
-    pub fn search(vec: &Vec<i32>, target: i32) -> (i32, i32) {
-        for i in 0..vec.len() {
+    pub fn search(vec: &[i32], target: i32) -> (i32, i32) {
+        for (i, _) in vec.iter().enumerate() {
             if vec[i] == target {
                 return (i as i32, i as i32 + 1);
             }
@@ -13,9 +13,10 @@ pub mod binary {
     use std::usize;
 
     use crate::algos::sorting;
+    use std::cmp;
 
-    pub fn search(vec: &Vec<i32>, target: i32) -> (i32, i32) {
-        let mut sorted_vec = vec.clone();
+    pub fn search(vec: &[i32], target: i32) -> (i32, i32) {
+        let mut sorted_vec = vec.to_owned();
         sorting::quick::sort(&mut sorted_vec);
 
         let mut l: i32 = 0;
@@ -25,14 +26,12 @@ pub mod binary {
             count += 1;
             let m: usize = (l as usize + r as usize) / 2;
 
-            if sorted_vec[m] < target {
-                l = m as i32 + 1;
-            } else if sorted_vec[m] > target {
-                r = m as i32 - 1;
-            } else {
-                return (m as i32, count);
+            match sorted_vec[m].cmp(&target) {
+                cmp::Ordering::Less => l = m as i32 + 1,
+                cmp::Ordering::Greater => r = m as i32 - 1,
+                cmp::Ordering::Equal => return (m as i32, count),
             }
         }
-        return (-1, -1);
+        (-1, -1)
     }
 }
